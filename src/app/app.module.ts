169bfedd658122ebf1b235fe9d9de9eb654d74bd
Phil <phil@metrodesk.com>
2017-01-06 16:23:46 +0000

    Ionic2 full app

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { SettingsPage } from '../pages/settings/settings';
+import { RedditsPage } from '../pages/reddits/reddits';
+import { DetailsPage } from '../pages/details/details';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    RedditsPage,
+    SettingsPage,
+    TabsPage,
+    DetailsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    RedditsPage,
+    SettingsPage,
+    AboutPage,
+    TabsPage,
+    DetailsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/pages/settings/settings.ts
@@ -0,0 +1,42 @@
+
+import { Component } from '@angular/core';
+import { RedditService } from '../../app/services/reddits.services';
+import { NavController } from 'ionic-angular';
+import { RedditsPage } from '../reddits/reddits';
+
+@Component({
+  selector: 'settings',
+  templateUrl: 'settings.html'
+})
+export class SettingsPage {
+	category:any;
+	limit:any;
+
+  constructor(public navCtrl: NavController, private redditService :RedditService) {
+  	//initialise
+  	this.getDefault();
+  }
+
+  getDefault() {
+  	if(localStorage.getItem('category') != null) {
+  		this.category = localStorage.getItem('category')
+  	} else {
+  		this.category = "sports"
+  	}
+
+  	if(localStorage.getItem('limit') != null) {
+  		this.limit = localStorage.getItem('limit')
+  	} else {
+  		this.limit = 10
+  	}
+  }
+
+  setDefaults() {
+  	localStorage.setItem('category', this.category);
+  	localStorage.setItem('limit', this.limit);
+  	this.navCtrl.push(RedditsPage)
+  }
+
+
+}
+
